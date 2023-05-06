e4934474751aa49c99baf9fd9a8f368aa81c83e1
kyoapps-kyo <kyo@kyoapps.com>
2023-05-06 12:33:44 +0800

    update: 回车

--- install-oh-my-zsh-plugins.sh
+++ install-oh-my-zsh-plugins.sh
@@ -4,10 +4,10 @@ CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${CURRENT_DIR}/common/variables.sh
 
 
-echo -e "${GREEN}安装autosuggestions && highlighting插件${RESET} /n"
+echo -e "\n\n${GREEN}...安装autosuggestions && highlighting插件...${RESET}\n\n"
 
 if [ -d ~/.oh-my-zsh/plugins/zsh-autosuggestions ]; then
-    echo -e "${RED}zsh-autosuggestions exists.${RESET} /n"
+    echo -e "${RED}...zsh-autosuggestions exists...${RESET}\n\n"
 else
     # 安装zsh-autosuggestions插件
     git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
@@ -17,7 +17,7 @@ fi
 
 
 if [ -d ~/.oh-my-zsh/plugins/zsh-syntax-highlighting ]; then
-    echo -e "${RED}zsh-syntax-highlighting exists.${RESET} /n"
+    echo -e "${RED}...zsh-syntax-highlighting exists...${RESET}\n\n"
 else
     # 安装zsh-syntax-highlighting插件
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
@@ -26,4 +26,4 @@ else
 fi
 
  # 重新加载zsh配置文件
-echo -e "${BLUE_LIGHT}请执行source ~/.zshrc${RESET}"
+echo -e "${BLUE_LIGHT}请执行: source ~/.zshrc${RESET}"
--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -5,13 +5,13 @@ CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${CURRENT_DIR}/common/variables.sh
 
 # 安装zsh
-echo -e "${GREEN}安装zsh......${RESET} /n"
+echo -e "\n\n${GREEN}安装zsh......${RESET}\n\n"
 sudo apt-get install -y zsh curl
 
 if [ -d ~/.oh-my-zsh ]; then
-    echo -e "${RED}oh-my-zsh 已经存在......${RESET} /n"
+    echo -e "${RED}oh-my-zsh 已经存在......${RESET}\n\n"
 else
-    echo -e "${GREEN}安装oh-my-zsh......${RESET} /n"
+    echo -e "${GREEN}安装oh-my-zsh......${RESET}"
     # 安装oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
