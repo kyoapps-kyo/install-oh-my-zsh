0a36f08704fb3e4478852d1b3bcb66a612217e5c
kyoapps-kyo <kyo@kyoapps.com>
2023-05-06 11:52:24 +0800

    create: 插件安装脚本

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -20,27 +20,3 @@ fi
 # echo "设置zsh为默认shell"
 # 设置zsh为默认shell
 # chsh -s $(which zsh)
-
-echo -e "${GREEN}安装autosuggestions && highlighting插件${RESET}"
-
-if [ -d ~/.oh-my-zsh/plugins/zsh-autosuggestions ]; then
-    echo "zsh-autosuggestions exists."
-else
-    # 安装zsh-autosuggestions插件
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-     # 使插件生效
-    echo "source ~/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-fi
-
-
-if [ -d ~/.oh-my-zsh/plugins/zsh-syntax-highlighting ]; then
-    echo "zsh-syntax-highlighting exists."
-else
-    # 安装zsh-syntax-highlighting插件
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-    # 使插件生效
-    echo "source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-fi
-
- # 重新加载zsh配置文件
-echo "请执行source ~/.zshrc"
